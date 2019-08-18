f9b87ce04982e09e36425d4405e0a792b1bb3b2e
tonysbabu <tonysbabu@gmail.com>
2019-08-18 13:38:45 +0530

    modified package.json

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,5 +1,6 @@
 module.exports = (app) => {
 const controller = require('../controllers/controller.js');
+app.get('/', (req, res) => res.send(`Welcome to web scraper app`));
 app.post('/scrape-url', controller.scrapeURL);
 
 }
